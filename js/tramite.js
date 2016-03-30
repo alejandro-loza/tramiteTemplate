b8e83411ae7b29d809aa5025bc5c987b0f39feb8
pinky <orochi_alex@hotmail.com>
2016-03-30 17:52:48 -0600

    maqueta de login y formulario

--- js/tramite.js
+++ js/tramite.js
@@ -63,11 +63,8 @@ tramite = {
         var controller = this;
         this.step++;
         var current_step = this.step;
-        //console.log('ENTER ------->');
-        //$('section').fadeOut('500', function() {
         $('section').addClass('hidden');
         $('.' + controller.steps[current_step]).removeClass('hidden');        
-        //});
         if (controller.steps[current_step] == 'confirmation') {
             setTimeout(function () {
                 $('.confirmation .loader').hide('fast', function () {
@@ -83,38 +80,7 @@ tramite = {
     search: function () {
 
         var controller = this;
-        var curp = document.getElementById("CURP").value;
-
-        var shaObj = new jsSHA("SHA-1", "TEXT");
-        shaObj.update("PRESIDENCIA" + curp);
-        var hash = shaObj.getHash("HEX");
-
-        $.ajax({
-            url: this.url + this.wsCURP,
-            type: 'POST',
-            contentType: "application/json",
-            dataType: 'json',
-            crossOrigin: true,
-            crossDomain: true,
-            data: JSON.stringify({
-                "dependencia": "PRESIDENCIA",
-                //"curp": "HUTR840605HDFRRB00",
-                "curp": curp,
-                //hash": "b09ffbf37a4a284c9fdff4f2c5532f22ae454486",
-                "hash": hash,
-                "isImg": 0
-            }),
-            success: function (response) {
-                //steps: selected step + 1
-                controller.actaResponse = response.return.nacimientos[0];
-                //alert("Respuesta: " + actaResponse);
-
-            },
-            error: function (e) {
-                alert("Error: " + e);
-            }
-        });
-
+//TODO ws de logeo
         $('#folios').removeClass('hidden').show('slow');
 
         setTimeout(function () {
@@ -123,71 +89,5 @@ tramite = {
             });
         }, 1000);
     },
-    addActa: function (actaResponse) {
-
-        var camposActa = ['folio', 'oficialia', 'foja', 'libro', 'noActa', 'fechaNacimiento', 'curp', 'estadoNacNombre', 'nombre', 'primerApellido', 'segundoApellido', 'sexo', 'vivoMuerto'];
-        for (x = 0; x < camposActa.length; x++) {
-            var acta = $("<p style='font-size: 8pt; text-align: center;'/>")
-            acta.text(camposActa[x] + ':' + actaResponse[camposActa[x]])
-            acta.appendTo('.detalle-acta');
-        }
-
-
-    },
-    selectFolio: function (element) {
-        $(element).css({'background-color': '#999'})
-        $('.next_step').removeClass('hidden');
-    },
-    preview: function () {
-        // TODO ajax to get preview acta
-        alert('ajua');
-    },
-    dataError: function () {
-
-    },
-    checkout: function () {
-        //$('section').addClass('hidden');
-        //$('.checkout').removeClass('hidden');
-    },
-    confirmation: function () {
-        //$('section').addClass('hidden');
-        //$('.confirmation').removeClass('hidden');
-        $('.exito').removeClass('hidden');
-    },
-    download: function () {
-
-    	var pdf;
-        var controller = this;
-        var folio = JSON.parse(window.localStorage[controller.ls]).json.folio;
-        $('.download-loading').removeClass('hidden');
-
-    	$.ajax({
-
-            url: this.url + this.wsPDF,
-            type: 'POST',
-            contentType: "application/json",
-            dataType: 'json',
-            crossOrigin: true,
-            crossDomain: true,
-            data: JSON.stringify({               	
-					"arg0": folio
-            }),
-            success: function (response) {
-                pdf = response.return.Reporte;
-                setTimeout(function(){
-                	var url = 'data:aplication/pdf;base64,' + pdf ;
-                	window.open(url);
-                    console.log('start download');
-                    $('.download-loading').addClass('hidden');
-                },3000);
-
-            },
-            error: function (e) {
-                alert("Error: " + e);
-            }
-        });
-
-
-
-    },
+   
 }
